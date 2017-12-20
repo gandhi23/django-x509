006a980a341c0a5defd23331ed3f449b3988a6e5
gandhi23 <34238569+gandhi23@users.noreply.github.com>
2017-12-20 21:26:36 +0530

    Update urls.py

--- tests/urls.py
+++ tests/urls.py
@@ -1,6 +1,6 @@
-from django.urls import include, re_path
 from django.contrib import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.urls import include, re_path
 
 admin.autodiscover()
 
