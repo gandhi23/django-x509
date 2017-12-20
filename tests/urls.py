7668095588e1c764645a7781d47746a65b3fcef2
gandhi23 <34238569+gandhi23@users.noreply.github.com>
2017-12-20 20:39:08 +0530

    Update urls.py

--- tests/urls.py
+++ tests/urls.py
@@ -1,15 +1,16 @@
+from django.conf.urls import url, include
 from django.contrib import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-from django.urls import include, path
 
 admin.autodiscover()
 
 
-app_name = 'x509'
+app_name = 'admin'
 urlpatterns = [
-    path('admin', admin.site.urls),
-    # django-x509.urlS
-    path('', include('django_x509.urls', namespace='x509')),
+    url(r'^admin/', admin.site.urls),
+    # django-x509.urls
+    # keep the namespace argument unchanged
+    url(r'^', include('django_x509.urls', name='x509')),
 ]
 
 urlpatterns += staticfiles_urlpatterns()
