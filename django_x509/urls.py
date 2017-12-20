7bf1516b10d0c0f96df801a329ac28c5311912e0
gandhi23 <34238569+gandhi23@users.noreply.github.com>
2017-12-20 21:40:54 +0530

    Update urls.py

--- django_x509/urls.py
+++ django_x509/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 
 from . import views
 
-app_name='django_x509'
+app_name = 'django_x509'
 urlpatterns = [
     path(r'^x509/ca/(?P<pk>[^/]+).crl$', views.crl, name='crl'),
 ]
