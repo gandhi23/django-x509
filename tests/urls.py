aa3c5a35f532fd91e53988cdf8a03a5ff063ca62
gandhi23 <34238569+gandhi23@users.noreply.github.com>
2017-12-22 19:37:12 +0530

    Update urls.py

--- tests/urls.py
+++ tests/urls.py
@@ -5,7 +5,7 @@ from django.urls import include, re_path
 admin.autodiscover()
 
 urlpatterns = [
-    re_path(r'^', include('django_x509.urls')),
+    re_path(r'^', include('django_x509.urls', namespace="x509")),
     re_path(r'^admin/', admin.site.urls),
 ]
 
